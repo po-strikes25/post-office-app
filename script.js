f0265010bf0a7d9f50ed38134791772e641d035f
Shilpi <svvar31@gmail.com>
2023-08-05 23:22:17 +0530

    second upload

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
-const geoBtn = document.getElementById("btn");
-const home = document.getElementById("home-container");
+const geoButton = document.getElementById("btn");
+const homeContainer = document.getElementById("home-container");
 const addressContainer = document.getElementById("address-details-container");
 const officeContainer = document.getElementById("Post-office-container");
-const searchInput = document.getElementById("search-input");
+const searchItem = document.getElementById("search-input");
+
 window.onload = function () {
 
     const userIP = fetch("https://api64.ipify.org?format=json")
-        .then(response => response.json())
+        .then(res => res.json())
         .then(data => {
-            // set ip address to our page   
-            document.getElementsByClassName('ip_addr')[0].innerText = data.ip;
+            document.getElementsByClassName('ip-address')[0].innerText = data.ip;
             return data.ip;
         })
         .catch((error) => {
-            document.getElementsByClassName('ip_addr')[0].innerText = "0.0.0.0";
+            document.getElementsByClassName('ip-address')[0].innerText = "0.0.0.0";
             console.error('Error fetching data:', error);
             throw error;
         });
 
-    function createPostalCard(postOfficeBranches) {
+    // createPostCard function
+    function createPostCard(postOfficeBranches) {
         officeContainer.innerHTML = '';
         postOfficeBranches.forEach(branch => {
 
             const card = document.createElement('div');
             card.setAttribute('class', 'post-card');
             card.innerHTML = `
-            <p>Name: <span>${branch.Name}</span></p>
-            <p>Branch Type: <span>${branch.BranchType}</span></p>
-            <p>Delivery Status: <span>${branch.DeliveryStatus}</span></p>
-            <p>District: <span>${branch.District}</span></p>
-            <p>Division: <span>${branch.Division}</span></p>
+                <p>Name: <span>${branch.Name}</span></p>
+                <p>Branch Type: <span>${branch.BranchType}</span></p>
+                <p>Delivery Status: <span>${branch.DeliveryStatus}</span></p>
+                <p>District: <span>${branch.District}</span></p>
+                <p>Division: <span>${branch.Division}</span></p>
             `
             officeContainer.append(card);
         })
     }
+
+    // PostOffice function
     function searchPostOffice(data) {
-        let searchText = searchInput.value.trim().toLowerCase();
-        // m e n s
-        // Mens Casual Premium Slim Fit T-Shirts
+        let searchText = searchItem.value.trim().toLowerCase();
         if (searchText === '') {
-            createPostalCard(data);
+            createPostCard(data);
         }
         else {
             data = data.filter((office) => {
@@ -54,46 +55,49 @@ window.onload = function () {
                 }
             })
         }
-        createPostalCard(data);
+        createPostCard(data);
     }
-    geoBtn.addEventListener("click", async () => {
 
-        home.style.display = "none";
+    // geoButton Listener
+    geoButton.addEventListener("click", async () => {
+
+        homeContainer.style.display = "none";
         addressContainer.style.display = "block";
         try {
-            // Get user's IP address
-            const ipResponse = await userIP;
-            // Get user's location information from API
-            const response = await fetch(`https://ipinfo.io/${ipResponse}?token=4ecca9628f9433`);
-            const locationData = await response.json();
-            const postOfficeResponse = await fetch(`https://api.postalpincode.in/pincode/${locationData.postal}`);
-            const branches = await postOfficeResponse.json();
+            const responseIP = await userIP;
+            const data = await fetch(`https://ipinfo.io/${responseIP}?token=4ecca9628f9433`);
+            const locationData = await data.json();
+            const postOfficeData = await fetch(`https://api.postalpincode.in/pincode/${locationData.postal}`);
+            const branches = await postOfficeData.json();
 
-            document.getElementsByClassName('ip_addr')[1].innerText = locationData.ip;
-            console.log(locationData);
+            document.getElementsByClassName('ip-address')[1].innerText = locationData.ip;
             let lattiude = locationData.loc.split(',')[0];
             let longitude = locationData.loc.split(',')[1];
+            document.getElementById('region').innerText = locationData.region;
             document.getElementById('lat').innerText = lattiude;
             document.getElementById('lon').innerText = longitude;
             document.getElementById('city').innerText = locationData.city;
-            document.getElementById('reg').innerText = locationData.region;
-            document.getElementById('org').innerText = locationData.asn.name;
-            document.getElementById('host').innerText = locationData.hostname;
+            document.getElementById('organization').innerText = locationData.org;
+
+            document.getElementById('host').innerText = locationData.org.split(' ')[0];
             const mapLocation = document.getElementById('map-frame');
+
             mapLocation.setAttribute("src", `https://www.google.com/maps/embed/v1/place?q=${lattiude},${longitude}&key=AIzaSyDU3QvuVFlZ9PcWIU2JYAaO0il88WNa-V0`);
             document.getElementById('time-zone').innerText = locationData.timezone;
-            // datetime in "America/Chicago" timezone in the "en-US" locale
-            let chicago_datetime_str = new Date().toLocaleString("en-US", { timeZone: locationData.timezone });
-            document.getElementById('date-time-zone').innerText = chicago_datetime_str;
+
+            let datetime_string = new Date().toLocaleString("en-US", { timeZone: locationData.timezone });
+            document.getElementById('date-time-zone').innerText = datetime_string;
             document.getElementById('pincode').innerText = locationData.postal;
             document.getElementById('message').innerText = branches[0].Message;
+
             const postOffice = branches[0].PostOffice;
-            createPostalCard(postOffice);
-            searchInput.addEventListener('input', (event) => {
+            createPostCard(postOffice);
+
+            searchItem.addEventListener('input', (event) => {
                 searchPostOffice(postOffice);
             })
-        } catch (error) {
-            console.error('data failed to load...', error);
+        } catch (err) {
+            console.log('data failed to load...', err);
         }
     })
 }
